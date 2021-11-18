c1aafc510e9d1058b28ee107e57cede530624c21
Arlin-Mafra <arlinsantana@gmail.com>
2021-11-18 15:18:33 -0300

    header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import Logo from "../../assets/logo.svg";
 
-import { Container, Title } from "./styles";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
