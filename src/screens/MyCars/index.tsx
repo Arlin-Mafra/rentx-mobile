f7989454f6e80350ed60b2667f65ad0fa6a5ccd5
Arlin-Mafra <arlinsantana@gmail.com>
2021-12-05 20:06:42 -0300

    listing my cars

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StatusBar } from "react-native";
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
 import { CarDTO } from "../../dtos/CarDTO";
 import api from "../../services/api";
+import theme from "../../styles/theme";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsNumber,
+} from "./styles";
 
-export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
-  const [lopading, setLoading] = useState(false);
+interface CarsProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
+
+export function MyCars({ navigation }) {
+  const [cars, setCars] = useState<CarsProps[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchCars() {
@@ -21,5 +41,42 @@ export function MyCars() {
     }
     fetchCars();
   }, []);
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton
+          color={theme.colors.shape}
+          onPress={() => navigation.goBack()}
+        />
+        <Title>
+          Seus agendamentos,{"\n"}
+          estão aqui.
+        </Title>
+
+        <SubTitle>Conforto, segurança e praticidade.</SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsNumber>{cars.length}</AppointmentsNumber>
+        </Appointments>
+        {loading ? (
+          <Load />
+        ) : (
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => <Car data={item.car} />}
+          />
+        )}
+      </Content>
+    </Container>
+  );
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,57 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  
+    flex: 1;
+    align-items: center;
+    background-color: ${({theme}) => theme.colors.background_primary};
 `;
+
+export const Header = styled.View`
+    width: 100%;
+    height: 375px;
+    background-color: ${({theme})=> theme.colors.header};
+
+    justify-content: center;
+    padding: 25px;
+
+    padding-top: ${getStatusBarHeight() + 30}px;
+`;
+export const  Title = styled.Text`
+    color: ${({theme})=> theme.colors.shape};
+    font-family: ${({theme})=> theme.fonts.secondary_600};
+    font-size: ${RFValue(28)}px;
+
+    
+    margin-top: 24px;
+`;
+export const  SubTitle = styled.Text`
+    color: ${({theme})=> theme.colors.shape};
+    font-family: ${({theme})=> theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+    margin-top: 24px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    width: 100%;
+    padding: 0 16px;
+`;
+export const Appointments = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 24px 0;
+`;
+export const AppointmentsTitle = styled.Text`
+    color: ${({theme})=> theme.colors.text};
+    font-family: ${({theme})=> theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+`;
+export const AppointmentsNumber = styled.Text`
+    color: ${({theme})=> theme.colors.title};
+    font-family: ${({theme})=> theme.fonts.secondary_500};
+    font-size: ${RFValue(15)}px;
+`;
+
+
