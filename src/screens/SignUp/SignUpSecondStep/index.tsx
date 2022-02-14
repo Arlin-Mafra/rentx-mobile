f2eddd96a0878dc1b48c84e397beb078da0702d6
Arlin-Mafra <arlinsantana@gmail.com>
2022-02-14 19:03:28 -0300

    integration signup with api

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -7,9 +7,18 @@ import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { Button } from "../../../components/Button";
 import { PasswordInput } from "../../../components/PasswordInput";
+import { api } from "../../../services/api";
 import theme from "../../../styles/theme";
 
-import { Container, Header, Steps, Form, FormTitle } from "./styles";
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle,
+} from "./styles";
 
 type Props = NativeStackScreenProps<any, " SignUpSecondStep">;
 
@@ -30,12 +39,24 @@ export function SignUpSecondStep({ navigation }: Props) {
     navigation.goBack();
   }
 
-  function handleRegister() {
+  async function handleRegister() {
     if (!password || !passwordConfirm) {
       Alert.alert("Opa!", "A senha e a confirmação são obrigatórias!");
     }
     if (password != passwordConfirm) {
       Alert.alert("Opa!", "A senha e a confirmação não conferem!");
+    }
+
+    try {
+      await api.post("/users", {
+        name: user.name,
+        email: user.email,
+        driver_license: user.driverLicense,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Opa!", "Algo deu errado na criação da sua conta.");
     }
     navigation.navigate("Confirmation", {
       title: "Conta criada!",
@@ -54,6 +75,11 @@ export function SignUpSecondStep({ navigation }: Props) {
               <Bullet active={false} />
             </Steps>
           </Header>
+          <Title>Crie sua{"\n"}conta</Title>
+          <SubTitle>
+            Faça seu cadastro de{"\n"}
+            forma rápida e fácil.
+          </SubTitle>
           <Form>
             <FormTitle>2. Senha</FormTitle>
             <PasswordInput
